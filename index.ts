dc621f9fdc1d79e13baabc084d9f90a6368a7586
Shyam Mahanta <smahanta118@gmail.com>
2024-02-24 23:45:14 +0530

    added some consoles

--- index.ts
+++ index.ts
@@ -1,17 +1,20 @@
 // import express from "express";
 // import smtp from "smtp-server";
 import { SMTPServer } from "smtp-server";
-import 'dotenv/config'
+import "dotenv/config";
 
 const server = new SMTPServer({
   allowInsecureAuth: true,
   onConnect(session, cb) {
+    console.log("session connected ", session.id);
     cb();
   },
   onMailFrom(address, session, callback) {
+    console.log("Found address ", address.address);
     callback();
   },
   onRcptTo(address, session, callback) {
+    console.log("Received to ", address.address);
     callback();
   },
   onData(stream, session, callback) {
